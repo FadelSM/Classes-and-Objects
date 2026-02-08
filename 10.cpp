4d7bbbb32144cd7009f31005b62e67b4b7e88e22
FadelSM <rasyahstore8888@gmail.com>
2026-02-08 13:25:42 +0700

    Implement Student class and score calculation
    Added Student class to manage student scores and input.

--- 10.cpp
+++ 10.cpp
@@ -6,7 +6,25 @@
 #include <cassert>
 using namespace std;
 
-// Write your Student class here
+class Student {
+public:
+    vector<int> scores;
+
+    void input() {
+        scores.resize(5);
+        for (int i = 0; i < 5; i++) {
+            cin >> scores[i];
+        }
+    }
+
+    int calculateTotalScore() {
+        int sum = 0;
+        for (int x : scores) {
+            sum += x;
+        }
+        return sum;
+    }
+};
 
 int main() {
     int n; // number of students
